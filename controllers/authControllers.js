25cb5063e4291ca502c696696486f99c8583a608
Pavlo Lozynskyi <p.lozynskyi@gmail.com>
2023-05-13 21:57:00 +0300

    add login

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,6 @@
 const { catchAsync } = require('../utils/catchAsync');
 
-const { signupService } = require('./../services/authServices');
+const { signupService, loginService } = require('./../services/authServices');
 
 let signup = async (req, res) => {
   const user = await signupService(req.body);
@@ -9,7 +9,10 @@ let signup = async (req, res) => {
 
 signup = catchAsync(signup);
 
-let login = async (req, res) => {};
+let login = async (req, res) => {
+  const { user, accessToken } = await loginService(req.body);
+  res.status(200).json({ user, token: accessToken });
+};
 
 login = catchAsync(login);
 
--- services/authServices.js
+++ services/authServices.js
@@ -1,6 +1,10 @@
+const bcrypt = require('bcrypt');
+
 const { User } = require('./../models/User');
 
 const { HttpError } = require('./../utils/HttpError');
+
+const { createTokens } = require('./../utils/createTokens');
 
 const signupService = async body => {
   const { email } = body;
@@ -15,4 +19,23 @@ const signupService = async body => {
   return newUser;
 };
 
-module.exports = { signupService };
+const loginService = async body => {
+  const { email, password } = body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new HttpError(401, 'Not authorized');
+  }
+
+  const isPasswordCorrect = bcrypt.compare(password, user.password);
+  if (!isPasswordCorrect) {
+    throw new HttpError(401, 'Not authorized');
+  }
+
+  const { accessToken, refreshToken } = createTokens({ userId: user._id });
+
+  await User.findByIdAndUpdate(user._id, { refresh_token: refreshToken });
+
+  return { user, accessToken };
+};
+
+module.exports = { signupService, loginService };
